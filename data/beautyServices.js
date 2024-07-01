8ad0f88634a42d0917d708e603298f0285e2c15b
Carlos Adrian Navarrete Ramirez <carlos.navarrete@tecsup.edu.pe>
2024-06-30 19:04:35 -0500

    backend commit

---
+++ data/beautyServices.js
@@ -0,0 +1,63 @@
+export const services = [
+    
+    { 
+        "name": "Consulta General", 
+        "price": 30 
+    },
+    { 
+        "name": "Vacunación", 
+        "price": 25 
+    },
+    { 
+        "name": "Desparasitación", 
+        "price": 20 
+    },
+    { 
+        "name": "Limpieza Dental", 
+        "price": 50 
+    },
+    { 
+        "name": "Radiografía", 
+        "price": 40 
+    },
+    { 
+        "name": "Ecografía", 
+        "price": 70 
+    },
+    { 
+        "name": "Castración/ Esterilización", 
+        "price": 100 
+    },
+    { 
+        "name": "Consulta de Emergencia", 
+        "price": 60 
+    },
+    { 
+        "name": "Cirugía Menor", 
+        "price": 150 
+    },
+    { 
+        "name": "Hospitalización por Día", 
+        "price": 80 
+    },
+    { 
+        "name": "Análisis de Sangre", 
+        "price": 35 
+    },
+    { 
+        "name": "Microchip", 
+        "price": 45 
+    },
+    { 
+        "name": "Peluquería Canina/Felina", 
+        "price": 30 
+    },
+    { 
+        "name": "Baño y Corte de Uñas", 
+        "price": 25 
+    },
+    { 
+        "name": "Consulta Especializada (Dermatología, Cardiología, etc.)", 
+        "price": 50 
+    }
+  ];
